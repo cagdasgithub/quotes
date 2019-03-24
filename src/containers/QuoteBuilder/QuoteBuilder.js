b9265402282e0fef11c94d39aa07658ddfc495af
cessiz2 <cagdas.essiz@gmail.com>
2019-03-23 23:57:57 -0400

    add functionality

--- src/containers/QuoteBuilder/QuoteBuilder.js
+++ src/containers/QuoteBuilder/QuoteBuilder.js
@@ -1,18 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useRef } from "react";
 import axios from "axios";
 import Quote from "../../components/Quote/Quote";
 
 const QuoteBuilder = () => {
   const [quotes, setQuotes] = useState([]);
+  const textRef = useRef();
+  const authorRef = useRef();
+
+  const [item, dispatch] = useReducer((state, action) => {
+    console.log(action);
+    switch (action.type) {
+      case "add":
+        return (
+          {
+            text: action.text,
+            author: action.author
+          });
+      default:
+        return state;
+    }
+  }, {});
 
   useEffect(() => {
+    let fetchedQuotes = [];
     axios
       .get("https://motivator-5a144.firebaseio.com/quotes.json")
       .then(res => {
-        setQuotes(res.data);
-        //console.log(res.data);
+        for (let key in res.data) {
+          fetchedQuotes.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        setQuotes(fetchedQuotes);
       });
   }, [quotes]);
+
+  useEffect(() => {
+    console.log(item);
+    axios.post("https://motivator-5a144.firebaseio.com/quotes.json", item);
+  }, [item]);
+
+  function addQuote(event) {
+    event.preventDefault();
+
+    dispatch({
+      type: "add",
+      text: textRef.current.value,
+      author: authorRef.current.value
+    });
+  }
 
   return (
     <React.Fragment>
@@ -26,8 +63,11 @@ const QuoteBuilder = () => {
           />
         );
       })}
-
-      <button>Add Quote</button>
+      <p>Title:</p>
+      <input ref={textRef} />
+      <p>author:</p>
+      <input ref={authorRef} />
+      <button onClick={addQuote}>Add Quote</button>
     </React.Fragment>
   );
 };
